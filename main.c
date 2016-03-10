709752bf1a5d6ae9cc846374544be0fa443ea263
Carlos Tonatihu <carlostonatihu@gmail.com>
2016-03-09 20:33:02 -0600

    Creando un metodo para iniciar las variables

--- main.c
+++ main.c
@@ -9,15 +9,39 @@ Conjunto resultado;
 Conjunto *p;
 Conjunto bueno;
 Carteciano PCR;
+int prueba;
+int iniciar_variables_main();
 
 int
 main (void)
+{
+  iniciar_variables_main();
+  potencia(&A, &p);
+  //printf("%d\n", (p+30)->elementos[4]);
+  prueba = interseccion (&A, &B, &resultado);
+  printf("Es vacio D: %d\n", es_vacio(&D));
+  printf("Es vacio resultado: %d\n", es_vacio(&resultado));
+
+  int i;
+  for (i = 0; i < cardinalidad(&resultado); i++)
+  {
+    printf ("Elementos: %d\n", resultado.elementos[i]);
+  }
+  // Terminan pruebas de interseccion y es vacio
+  //Pruebas de producto cartesiano:
+  //producto_carteciano (A, B, &PCR);
+  //Checo que el apuntador sea correcto
+  //printf ("%d\n Final de producto cartesiano", PCR.cardinalidad);
+  return 0;
+}
+
+int
+iniciar_variables_main(void)
 {
   // Inician pruebas de interseccion y es vacio
   crear_vacio (&B);
   crear_vacio (&resultado);
   crear_vacio (&A);
-  int prueba;
 
   A.elementos[0] = 1;
   A.elementos[1] = 3;
@@ -33,23 +57,5 @@ main (void)
   B.cardinalidad = 4;
   A.cardinalidad = 5;
 
-  potencia(&A, &p);
-  //printf("%d\n", (p+30)->elementos[4]);
-
-  prueba = interseccion (&A, &B, &resultado);
-  printf("Es vacio D: %d\n", es_vacio(&D));
-  printf("Es vacio resultado: %d\n", es_vacio(&resultado));
-
-  int i;
-  for (i = 0; i < cardinalidad(&resultado); i++)
-  {
-    printf ("Elementos: %d\n", resultado.elementos[i]);
-  }
-  // Terminan pruebas de interseccion y es vacio
-
-  //Pruebas de producto cartesiano:
-  //producto_carteciano (A, B, &PCR);
-  //Checo que el apuntador sea correcto
-  //printf ("%d\n Final de producto cartesiano", PCR.cardinalidad);
   return 0;
 }
